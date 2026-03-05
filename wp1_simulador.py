1a7b1fa2a2047ac21d74f4626d21f3a2c2a654a5
Riki-233 <rsitjes2006@gmail.com>
2026-03-05 13:16:17 +0100

    Delete wp1_simulador.py

--- wp1_simulador.py
+++
@@ -1,4 +0,0 @@
-import math
-
-v_RC = math.sqrt((Ct1*(1-(hp/Ct2)+Ct*hp*hp) + math.sqrt((Ct1 * (1-(hp/Ct2)+Ct*hp*hp) + 12 * Cdo * m * m * g * g * Cd2)/3* d * S * Cdo)
-v_angle = ((4 * ((m*g)**2) * Cd2)/((d * S)**2) * Cdo) ** 1/4
